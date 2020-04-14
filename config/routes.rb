615b6b14d159dbc777480e1d5728fed55b037703
CaioFML <caio.felipemotta@hotmail.com>
2020-04-14 00:33:28 -0300

    Criando o endpoint de discovery

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root "home#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
