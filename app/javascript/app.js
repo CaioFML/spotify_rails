7a99b73389a1acecf65978566efec3dda42ffed6
CaioFML <caio.felipemotta@hotmail.com>
2020-04-16 19:30:19 -0300

    Criando o menu superior

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button } from 'react-bulma-components';
 import 'app.scss';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import Menu from './components/common/menu';
 
 const App = () => {
   return(
     <Fragment>
       <BrowserRouter>
-        <Routes/>
+        <Menu />
+        <Routes />
       </BrowserRouter>
     </Fragment>
   );
