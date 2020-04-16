81e1882ed051c255454cedec6780cff71c3f85a2
CaioFML <caio.felipemotta@hotmail.com>
2020-04-16 18:36:16 -0300

    Incluindo o Bulma

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
 
 const App = () => {
   return(
     <Fragment>
-      Hello APP
+      <Button color="primary">Testando o Bulma</Button>
     </Fragment>
   )
 }
