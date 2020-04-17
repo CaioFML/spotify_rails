603fa67595b65378db860500a4cb328514e3ab10
CaioFML <caio.felipemotta@hotmail.com>
2020-04-17 20:04:48 -0300

    Criando a página Discovery

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import Discovery from '../../components/discovery';
 import NavbarFooter from '../../components/common/navbar_footer';
 
 const DiscoveryScreen = () => {
   return(
     <Fragment>
-      <div>Discovery Screen</div>
-      <NavbarFooter/>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
+      <NavbarFooter />
     </Fragment>
-  )
+  );
 }
 
 export default DiscoveryScreen;
