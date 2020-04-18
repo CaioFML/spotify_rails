23273793bc18aa82dc570ece5dad1838ef705f1d
CaioFML <caio.felipemotta@hotmail.com>
2020-04-18 18:39:53 -0300

    Criando a página de favoritos

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
 import NavbarFooter from '../../components/common/navbar_footer';
+import SectionWrapper from '../../components/common/section_wrapper';
+import { Heading } from 'react-bulma-components';
+import Favorites from '../../components/favorites';
 
 const FavoritesScreen = () => {
-  return(
+  return (
     <Fragment>
-      <div>Favorites Screen</div>
-      <NavbarFooter/>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Favoritos</Heading>
+        <Favorites />
+      </SectionWrapper>
+      <NavbarFooter />
     </Fragment>
   )
 }
