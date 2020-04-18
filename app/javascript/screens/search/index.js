760059d1b9025fd77cbd98b4e1c38b6c6dc4187d
CaioFML <caio.felipemotta@hotmail.com>
2020-04-18 17:37:51 -0300

    Criando a página de Search

--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import { Heading } from 'react-bulma-components';
+import Search from '../../components/search';
 import NavbarFooter from '../../components/common/navbar_footer';
 
+
 const SearchScreen = () => {
-  return(
+  return (
     <Fragment>
-      <div>Search Screen</div>
-      <NavbarFooter/>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Buscar</Heading>
+        <Search />
+      </SectionWrapper>
+      <NavbarFooter />
     </Fragment>
-  )
+  );
 }
 
 export default SearchScreen;
